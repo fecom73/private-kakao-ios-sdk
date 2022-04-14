e0f57c6868f492d9fc3662eee7b7d4c4a3e78ade
lucas.arts <lucas.arts@kakaocorp.com>
2022-04-14 12:21:54 +0900

    모든 모듈 매니패스트에 추가

--- Package.swift
+++ Package.swift
@@ -16,28 +16,27 @@ let package = Package(
        .library(
            name: "KakaoSDKCommon",
            targets: ["KakaoSDKCommon"]),
-    //    .library(
-    //        name: "KakaoSDKAuth",
-    //        targets: ["KakaoSDKAuth"]),
-    //    .library(
-    //        name: "KakaoSDKUser",
-    //        targets: ["KakaoSDKUser"]),
-    //    .library(
-    //        name: "KakaoSDKTalk",
-    //        targets: ["KakaoSDKTalk"]),
-    //    .library(
-    //        name: "KakaoSDKStory",
-    //        targets: ["KakaoSDKStory"]),
-    //    .library(
-    //        name: "KakaoSDKLink",
-    //        targets: ["KakaoSDKLink"]),
-    //    .library(
-    //        name: "KakaoSDKNavi",
-    //        targets: ["KakaoSDKNavi"]),
-    //    .library(
-    //        name: "KakaoSDKTemplate",
-    //        targets: ["KakaoSDKTemplate"])
-        
+       .library(
+           name: "KakaoSDKAuth",
+           targets: ["KakaoSDKAuth"]),
+       .library(
+           name: "KakaoSDKUser",
+           targets: ["KakaoSDKUser"]),
+       .library(
+           name: "KakaoSDKTalk",
+           targets: ["KakaoSDKTalk"]),
+       .library(
+           name: "KakaoSDKStory",
+           targets: ["KakaoSDKStory"]),
+       .library(
+           name: "KakaoSDKLink",
+           targets: ["KakaoSDKLink"]),
+       .library(
+           name: "KakaoSDKNavi",
+           targets: ["KakaoSDKNavi"]),
+       .library(
+           name: "KakaoSDKTemplate",
+           targets: ["KakaoSDKTemplate"])        
     ],
 //    dependencies: [
 //        .package(name: "Alamofire",
@@ -61,34 +60,34 @@ let package = Package(
                      url:"https://github.com/fecom73/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKCommon_xcframework.zip",
                      checksum: "12cdb2a3a47b11ce1e07aedec62543423591f6a3e1482491ee209752ae15c89f"
        ),
-    //    .binaryTarget(name: "KakaoSDKAuth",
-    //                  url:"https://github.daumkakao.com/kops/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKAuth_xcframework.zip",
-    //                  checksum: "9c773ae8bb055ede9e4c6cfa42af3ef3f3ca690f455879553b4fc59f280eba73"
-    //    ),
-    //    .binaryTarget(name: "KakaoSDKUser",
-    //                  url:"https://github.daumkakao.com/kops/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKUser_xcframework.zip",
-    //                  checksum: "201ae8f31e41c97678e79482ee1b5e1663634235dbd7f3f8de5faa373ce47aa5"
-    //    ),
-    //    .binaryTarget(name: "KakaoSDKTalk",
-    //                  url:"https://github.daumkakao.com/kops/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKTalk_xcframework.zip",
-    //                  checksum: "c479542d3d8db50b01bff621d0106199b6cbeb56ac80c9c233885783e952bdfb"
-    //    ),
-    //    .binaryTarget(name: "KakaoSDKStory",
-    //                  url:"https://github.daumkakao.com/kops/private-kakao-partner-ios-sdk/releases/download/2.9.2/KakaoSDKStory_xcframework.zip",
-    //                  checksum: "8849d8787bfe329c32e53958a255ef6c78cb28276ddd89342a2cdaf327ed44ad"
-    //    ),
-    //    .binaryTarget(name: "KakaoSDKLink",
-    //                  url:"https://github.daumkakao.com/kops/private-kakao-partner-ios-sdk/releases/download/2.9.2/KakaoSDKLink_xcframework.zip",
-    //                  checksum: "9b069f4be448305085dcd722ed8d2af319ab32a5473f03c288a9108d731ccfdb"
-    //    ),
-    //    .binaryTarget(name: "KakaoSDKTemplate",
-    //                  url:"https://github.daumkakao.com/kops/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKTemplate.xcframework.zip",
-    //                  checksum: "1a27b46f95c2dfa9a9b8e255489cde75c8eb97377093a76f4251f4822a0f77da"
-    //    ),
-    //    .binaryTarget(name: "KakaoSDKNavi",
-    //                  url:"https://github.daumkakao.com/kops/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKNavi.xcframework.zip",
-    //                  checksum: "1a530fad34ecfbb87c000d8fe5360fa0c056ec9356d57397cabfb028134c8b5c"
-    //    )
+       .binaryTarget(name: "KakaoSDKAuth",
+                     url:"https://github.com/fecom73/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKAuth_xcframework.zip",
+                     checksum: "7a21605b26d2f5481bed50f766983811f7b49f6dbed370ec1f622519437f3024"
+       ),
+       .binaryTarget(name: "KakaoSDKUser",
+                     url:"https://github.com/fecom73/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKUser_xcframework.zip",
+                     checksum: "b7eeec3979e8276e13922b1cae984ebddb4de6df07971ca51ac7024609fd3f33"
+       ),
+       .binaryTarget(name: "KakaoSDKTalk",
+                     url:"https://github.com/fecom73/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKTalk_xcframework.zip",
+                     checksum: "6f8bdf2112776e5108e49411f6dac79832eccdc1cc632271f0016b675b77a65f"
+       ),
+       .binaryTarget(name: "KakaoSDKStory",
+                     url:"https://github.com/fecom73/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKStory_xcframework.zip",
+                     checksum: "0826299ded56d6de2adddf07d16e4e56476906da30e06a3a064d4fade6369a39"
+       ),
+       .binaryTarget(name: "KakaoSDKLink",
+                     url:"https://github.com/fecom73/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKLink_xcframework.zip",
+                     checksum: "20faec0371d5548218dcdba7a8d7256c6f66c52476e68feb7ddcd75499add212"
+       ),
+       .binaryTarget(name: "KakaoSDKTemplate",
+                     url:"https://github.com/fecom73/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKTemplate.xcframework.zip",
+                     checksum: "d3d73e4dad6a89d6f4697b51cc033406f5e9ffbee7e9850c7e0527e940f01166"
+       ),
+       .binaryTarget(name: "KakaoSDKNavi",
+                     url:"https://github.com/fecom73/private-kakao-ios-sdk/releases/download/2.9.2/KakaoSDKNavi.xcframework.zip",
+                     checksum: "74ab5e087226fec73dae21932552d629e2bde1c9dc8844baea8ded392cbb7438"
+       )
     ],
     swiftLanguageVersions: [
         .v5
